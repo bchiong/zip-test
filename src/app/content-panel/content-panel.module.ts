8bb4b17401cf61aa606d94ee289bdda0177d3fd2
bchiong <benjamin.chiong@worldnomadsgroup.com>
2019-11-24 18:21:52 +1100

    establish rought layout with bulma

--- src/app/content-panel/content-panel.module.ts
+++ src/app/content-panel/content-panel.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AppModule} from '../app.module';
-import {ContentCardComponent} from '../content-card/content-card.component';
+
 
 @NgModule({
   declarations: [
-    ContentCardComponent
   ],
   imports: [
     CommonModule,
